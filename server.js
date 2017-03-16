74367f2ae19ff9620d4affaa13e3848420ef3245
adam söderström <adamsdm1@gmail.com>
2017-03-16 14:13:12 +0100

    Added functionality to join rooms

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ var io = require('socket.io')(server);
 var cards = require('./data.json');
 
 
+// usernames which are currently connected to the chat
+var usernames = {};
+
+// rooms which are currently available in chat
+var rooms = ['room1','room2','room3', 'general'];
+
+
+
+
+//Debug
+function sendMsg(msg, room){
+  io.sockets.in(room).emit('debug', msg);
+}
+
+setInterval(function(){
+  var room = 'room1';
+  var msg = "Ur in " + room;
+  
+  sendMsg(msg,room);
+}, 1000);
+
+
+
+
 
 app.use(express.static(__dirname + '/public')); 
 app.get('/', function (req, res) {
@@ -19,7 +43,15 @@ app.get('/comp.html', function (req, res) {
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  // when the client emits 'adduser', this listens and executes
+  socket.on('adduser', function(username, room){
+    console.log("User "+username+" connected to room" + room);
+
+    socket.username = username;
+    socket.room = room;
+    usernames[username] = username;
+    socket.join(room);
+  });
 
   socket.on('disconnect', function(){
       console.log("A user disconnected");
@@ -35,7 +67,7 @@ io.on('connection', function(socket){
       var wc = cards["whiteCards"][wci];
       var str = bc["text"] + ' ('+ bc["pick"] + ')';
 
-      io.sockets.emit('update-black-card', str);
+      io.sockets.in(socket.room).emit('update-black-card', str);
       /*
       var bci = Math.round(Math.random()*(cards["blackCards"].length-1));
       var wci = Math.round(Math.random()*(cards["whiteCards"].length-1));
